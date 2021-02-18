6d831419e1d5d37f76b30946d22d63c8fdf1f1b6
wuzhe <wuzhe_@aliyun.com>
2021-02-18 15:38:27 +0800

    修改发布文件

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m '根路径加上/blog/'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:qdi5/qdi5.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:qdi5/blog.git master:gh-pages
+# git push -f git@github.com:qdi5/blog.git master:gh-pages
 
 cd -
